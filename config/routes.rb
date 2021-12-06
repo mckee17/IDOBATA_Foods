57ae1561e86ee7f932004e2d8d62774206e8609d
mckee17 <hayate0173@gmail.com>
2021-12-06 06:45:30 +0000

    [add]controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+
   devise_for :users, skip: [:passwords], controllers:{
     registrations: "public/registrations",
     sessions: "public/sessions"
@@ -7,6 +7,24 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations,:passwords], controllers:{
     sessions: "admin/sessions"
   }
-  
-  root to: 'homes#top'
+
+  root to: 'public/homes#top'
+
+  scope module: :public do
+    resources :foods
+    resources :compounds, only: [:create, :update, :destroy]
+    resources :food_compounds, only: [:create, :destroy]
+    resources :food_names, only: [:create, :update, :destroy]
+    resources :favorites, only: [:index, :create, :destroy]
+    resources :relationships, only: [:create, :destroy] do
+      collection do
+        get 'follow'
+      end
+    end
+    get 'searchs', to: 'searchs#search', as: 'search'
+  end
+
+  namespace :admin do
+    resources :genres, only: [:index,:edit, :create, :update]
+  end
 end
