7a25e6836b57007d74352c32b76c7daf48167f0b
mckee17 <hayate0173@gmail.com>
2021-12-12 21:57:42 +0000

    [add]search

--- app/controllers/public/searchs_controller.rb
+++ app/controllers/public/searchs_controller.rb
@@ -1,5 +1,24 @@
 class Public::SearchsController < ApplicationController
   def search
-    if params[:id] = 
+    case params[:id]
+      when "name"
+        @search = FoodName.where('name LIKE ?', "%#{params[:word]}%").order(created_at: :desc)
+        @search_count = 0
+        @search.each do |name|
+          @search_count += name.foods.count
+        end
+      when "compound"
+        @search = Compound.where('name LIKE ?', "%#{params[:word]}%").order(created_at: :desc)
+        @search_count = 0
+        @search.each do |compound|
+          @search_count += compound.foods.count
+        end
+      when "function"
+        @search_foods = Food.where('function LIKE ?', "%#{params[:word]}%").order(created_at: :desc)
+        @search_count = @search_foods.count
+      else
+        @search = {}
+        @seach_count = 0
+    end
   end
 end
